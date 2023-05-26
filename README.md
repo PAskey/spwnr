a6e520c00059fb0d01b76c0288478dfad7dc4ce0
Paul Askey <Paul.Askey@gofishbc.com>
2023-05-26 12:22:45 -0700

    Creating all paper functions and data

--- README.md
+++ README.md
@@ -19,5 +19,36 @@ The primary objective of this package is to make data available to others, and d
 
   * More detailed explanations of any of the functions below can be founds by typing ?function into the R console. E.g. ?GAUC. If you want to see the raw source code for any of the functions type View(function name) e.g. View(GAUC). If you would like to make changes of that code, ideally you go to my Github account to get the source code and create a branch with your potential edits.
 
-There are currently 3 data sets avaiable that were used in the published data analyses: 
+There are currently 4 data sets available that were used in the published data analyses: KO_cnts, KO_fence, Mission_ch, and spwnr. 
 
+These data sets are accessible in R by simply entering the dataset name. To bring the data into the RStudio environment, you can use data() at the prompt
+
+```R
+#These data sets are accessible in R by name.
+#To verify which data sets are included in a package use data():
+data(package = "spwnr")
+
+#The data sets are accessible by name. For example to see column names for spwnr dataset:
+names(KO_cnts)
+
+#To bring the data into your R environment use data() function. There is one dataset that has the same name as the package. If you pute spwn without "" in the data function it loads the data set.
+data(spwnr)
+
+```
+The data in the spwnr data set is the summarized data set with 3 different visual indices: PEAK_COUNT, TAUC and GAUC. All explained in ASkey et al. in review. The functions to calcualte TAUC and GAUC are included in this package by name and re-craetion of the summary data set can be accomplished as follows:
+
+```R
+library(dplyr)
+
+KO_cnts = KO_cnts %>%# mutate_if(is.numeric,as.integer)%>%
+  #mutate_if(is.character, as.factor)%>%
+  filter(SOURCE == 'OBSERVED')%>%
+  arrange(STREAM,YEAR,DOY)
+
+vis = KO_cnts%>%
+  group_by(STREAM_YR)%>%
+  dplyr::summarize(TAUC = TAUC(DOY, LIVE_COUNT), GAUC = GAUC(DOY, LIVE_COUNT), PC = max(LIVE_COUNT))
+
+vis = left_join(vis,KO_fence, by = "STREAM_YR")%>%select(-c(STREAM_YR, PEAK_VISUAL))
+
+```
--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,24 +1,54 @@
+#' This R code takes available raw data for Kokanee and Pink salmon and formats for analyses.
+#' Pink fence and AUC data is take from Millar et al. 2012 CJFAS
+#' Pink peak counts are taken from Hilborn et al. 1999 Table 1, or digitizing from figures in Millar et al. 2012.
+
+#' @title data_prep
+#' @name data_prep
+#' @keywords spwnr, data_prep
+#' @export
+#' @examples
+#' data_prep()
+#' @importFrom magrittr "%>%"
+#' @importFrom rlang .data
 
 
+data_prep <- function(){
 
+#Read in raw data from csv files in data-raw
+Mission_ch = read.csv("./Mission_channel.csv")
+KO_cnts = read.csv("./KO_cnts.csv")
+KO_fence = read.csv("./KO_fence.csv")
+Pink_data = read.csv("./Pink_data.csv")
+spwnr = read.csv("./spwnr.csv")
 
+#Summarize raw Kokanee counts into different visual indices.
+KO_data = KO_cnts%>%
+  dplyr::group_by(SPECIES = "KOKANEE", STREAM, YEAR)%>%
+  dplyr::summarize(PEAK_COUNT = max(LIVE_COUNT), TAUC = spwnr::TAUC(DOY,LIVE_COUNT), GAUC = spwnr::GAUC(DOY,LIVE_COUNT))%>%
+  ungroup()
 
-Mission_ch = read.csv("./data-raw/Mission_channel.csv")
-KO_cnts = read.csv("./data-raw/KO_cnts.csv")
-KO_fence = read.csv("./data-raw/KO_fence.csv")
-spwnr = read.csv("./data-raw/spwnr.csv")
+KO_data = dplyr::full_join(KO_data, KO_fence, by = c('STREAM',"YEAR"))
 
-#usethis::use_data(spwnr, overwrite = TRUE, fileEncoding="UTF-8-BOM")
-#use_data not working for some reason.
+#Pink data escapement needs to be converted to fish days to be comparable to KO
+Pink_data = Pink_data%>%
+  dplyr::mutate(SPECIES = "PINK", TAUC = TAUC_E*l*v, GAUC = GAUC_E*l*v)%>%
+  dplyr::select(-c(l,v,TAUC_E, HARR_E, GAUC_E))
 
-save(Mission_ch, file = "./data/Mission_ch.rda")
+spwnr = dplyr::full_join(KO_data,Pink_data)
 
-vis = KO_cnts%>%
-  group_by(STREAM_YR)%>%
-  dplyr::summarize(TAUC = TAUC(DOY, LIVE_COUNT), GAUC = GAUC(DOY, LIVE_COUNT), PC = max(LIVE_COUNT), top_3 = mean(LIVE_COUNT))
+#Create fitting groups for cross validation testing
+creek_groups = data.frame(
+  STREAM = c("CATHEAD", "CHENEGA", "COLDSTREAM", "COUNTESS", "HAWKINS",
+             "HAYDEN", "HERRING", "IRISH", "LOOMIS", "MIDDLE_VERNON", "PENTICTON",
+             "REDFISH"),
+  fit_group = c(1,1,1,1,2,2,2,3,3,2,3,3))
 
+spwnr = dplyr::left_join(spwnr, creek_groups, by = 'STREAM')%>%
+  dplyr::mutate(SPECIES = as.factor(SPECIES))
 
-vis = left_join(vis,fence, by = "STREAM_YR")%>%select(-c(STREAM_YR, PEAK_VISUAL))
+#SAve .rda files to data folder
+save(Mission_ch, file = "../data/Mission_ch.rda")
+save(spwnr, file = "../data/spwnr.rda")
+save(KO_cnts, file = "../data/KO_cnts.rda")
+}
 
-##______________________________________________________________________________________________
-#test commit
